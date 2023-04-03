d15d9681643c2f9bab7125020c9fec81538b7477
camargo <camargo.cac@gmail.com>
2023-04-02 21:33:48 -0700

    Add Pong v5

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import gymnasium as gym
 
-env = gym.make("LunarLander-v2", render_mode="human")
+env = gym.make("ALE/Pong-v5", render_mode="human")
 observation, info = env.reset()
 
 for _ in range(1000):
