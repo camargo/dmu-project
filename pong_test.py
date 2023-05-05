ab53fd438adaa4dc042710745c6bf6014cbd07d7
camargo <camargo.cac@gmail.com>
2023-05-05 12:08:39 -0700

    Add better stat collection for pong_test.py

--- pong_test.py
+++ pong_test.py
@@ -2,30 +2,44 @@ import gymnasium as gym
 from gymnasium.wrappers import RecordVideo
 from model_helpers import load_model
 from pong_train import PongPolicy, pong_observation
+from torch import manual_seed
 
 
 def main():
     policy: PongPolicy = load_model("2b9c7df2eca04bb49e31404f")
     policy.reset()
     record_video = False
+    use_seed = False
 
     if record_video:
         env = gym.make("ALE/Pong-v5", render_mode="rgb_array")
         env = RecordVideo(env, "videos", name_prefix="pong-agent")
         env.metadata["render_fps"] = 30
     else:
-        env = gym.make("ALE/Pong-v5", render_mode="human")
+        env = gym.make("ALE/Pong-v5")
 
     observation, _ = env.reset()
 
-    total_reward = 0
-    total_games = 0
+    if use_seed:
+        seed = 3
+        manual_seed(seed)
+        env.seed(seed)
 
-    for _ in range(50_000):
+    total_agent_wins = 0
+    total_computer_wins = 0
+    total_games = 0
+    agent_score = 0
+    computer_score = 0
+
+    for _ in range(1, 1_000_000):
         observation = pong_observation(observation)
         action = policy.action(observation)
         observation, reward, terminated, truncated, info = env.step(action)
-        total_reward += reward
+
+        if reward > 0:
+            agent_score += 1
+        elif reward < 0:
+            computer_score += 1
 
         if terminated or truncated:
             if record_video:
@@ -33,11 +47,24 @@ def main():
 
             total_games += 1
 
-            print(f"{info=}")
-            print(f"{total_reward=}")
-            print(f"{total_games=}")
+            if agent_score == 21:
+                total_agent_wins += 1
+            elif computer_score == 21:
+                total_computer_wins += 1
 
-            total_reward = 0
+            print(f"Agent score: {agent_score}, Computer score: {computer_score}")
+            print(f"Total games: {total_games}")
+            print(f"Total agent wins: {total_agent_wins}, Total computer wins: {total_computer_wins}")
+
+            if total_games % 100 == 0:
+                agent_percent_wins = (total_agent_wins / total_games) * 100
+                computer_percent_wins = (total_computer_wins / total_games) * 100
+                print(f"Agent percent wins: {agent_percent_wins:.2f}%")
+                print(f"Computer percent wins: {computer_percent_wins:.2f}%")
+
+            agent_score = 0
+            computer_score = 0
+
             env.reset()
             policy.reset()
 
